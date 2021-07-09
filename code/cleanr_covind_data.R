ffaef2d8acf6ebb9c76d6f052607bbb921413ade
Max Salvatore <maxsalvatore@Maxs-MacBook-Air.local>
2021-07-09 10:47:23 -0400

    push new test stack plots

--- code/cleanr_covind_data.R
+++ code/cleanr_covind_data.R
@@ -8,26 +8,27 @@ source(here("code", "functions", "functions.R"))
 set_seed <- 46342
 set.seed(set_seed)
 
-dat <- bind_rows(get_nat_counts(), get_state_counts()) %>%
-  left_join(
-    bind_rows(
-      get_nat_tests(),
-      get_state_tests()
-      ),
-    by = c("place", "date")) %>%
-  left_join(
-    get_r0(.) %>%
-      dplyr::rename(
-        r_est   = r,
-        r_lower = lower,
-        r_upper = upper
-        ), 
-    by = c("place", "date")) %>%
-  left_join(
-    pop %>%
-      dplyr::select(-population),
-    by = "place") %>%
-  mutate(tpr = daily_cases / daily_tests)
+dat <- get_all_data()
+# dat <- bind_rows(get_nat_counts(), get_state_counts()) %>%
+#   left_join(
+#     bind_rows(
+#       get_nat_tests(),
+#       get_state_tests()
+#       ),
+#     by = c("place", "date")) %>%
+#   left_join(
+#     get_r0(.) %>%
+#       dplyr::rename(
+#         r_est   = r,
+#         r_lower = lower,
+#         r_upper = upper
+#         ), 
+#     by = c("place", "date")) %>%
+#   left_join(
+#     pop %>%
+#       dplyr::select(-population),
+#     by = "place") %>%
+#   mutate(tpr = daily_cases / daily_tests)
 
 cfr    <- get_cfr(dat) %>% distinct()
 r0_est <- get_r_est(dat)
