fadac42627d0ea21d61a2da7df45343dca0c6c46
maxsal <mmsalva@umich.edu>
2022-06-06 08:41:07 -0400

    push plots

--- code/push_plots.R
+++ code/push_plots.R
@@ -18,6 +18,7 @@ tabs$cumulative
 
 source("code/clean_repo.R")
 
-git2r::add(rep = ".", path = NYLL, force = FALSE)
+git2r::add(repo = ".", path = ".", force = FALSE)
 git2r::commit(repo = ".", message = "push plots")
-git2r::push(name = "origin")
+git2r::push(name = "origin", refspec = "refs/heads/main")
+git2r::push(name = "gitlab", refspec = "refs/heads/main")
