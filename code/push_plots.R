64c94cce17698f4b30b0cf144d5e9ceda8dedafc
maxsal <mmsalva@umich.edu>
2022-06-08 07:52:07 -0400

    push plots

--- code/push_plots.R
+++ code/push_plots.R
@@ -1,6 +1,6 @@
 ally::libri(
   tidyverse, janitor, glue, ggtext, extrafont, here, httr, patchwork,
-  EpiEstim, maxsal/covid19india, data.table, scales, git2r 
+  EpiEstim, maxsal/covid19india, data.table, scales, git2r, gt 
 )
 
 source("functions.R")
